ba12f6a9e41b905d48e5a1067ec926037fbad92f
kanirolf <wiiopsunday@gmail.com>
2015-07-18 23:00:00 -0700

    Added ReminderManagerUser interface.

---
+++ app/src/main/java/lab/star/surf_iot2015/service_user/ReminderManagerUser.java
@@ -0,0 +1,7 @@
+package lab.star.surf_iot2015.service_user;
+
+import lab.star.surf_iot2015.ReminderManager;
+
+public interface ReminderManagerUser {
+    void onAcquireReminderManager(ReminderManager reminderManager);
+}
